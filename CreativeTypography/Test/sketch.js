ca2753411216f772df9f57720546c5add5b70b06
NikkiTian <tianmiaomiao994@gmail.com>
2025-02-21 11:45:51 -0500

    try to put 2 sketches together but failed

---
+++ CreativeTypography/Test/sketch.js
@@ -0,0 +1,41 @@
+// save this file as sketch.js
+// Sketch One
+var rect = function( p ) { // p could be any variable name
+    var x = 100; 
+    var y = 100;
+    p.setup = function() {
+      const myRectCanvas = p.createCanvas(400, 200);
+      myRectCanvas.parent("rectCanvas");
+    };
+  
+    p.draw = function() {
+      p.background(0);
+      p.fill(255);
+      p.rect(x,y,50,50);
+    };
+  };
+  
+  // Sketch Two
+  var circle = function( p ) { 
+    var x = 100.0; 
+    var y = 100; 
+    var speed = 2.5; 
+    p.setup = function() {
+        const myCircleCanvas = p.createCanvas(400, 200);
+        myCircleCanvas.parent("circleCanvas");
+    };
+  
+    p.draw = function() {
+      p.background(100);
+      p.fill(1);
+      x += speed; 
+      if(x > p.width){
+        x = 0; 
+      }
+      p.ellipse(x,y,50,50);
+  
+    };
+  }
+
+  var myp5 = new p5(rect);
+  var mySecondp5 = new p5(circle);
